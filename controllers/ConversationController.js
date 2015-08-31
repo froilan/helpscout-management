e07729829555113f41bd349ae0e49f63742c0360
froilan <froilan.co@synacy.com>
2015-09-01 00:47:40 +0800

    Add different status counts

--- controllers/ConversationController.js
+++ controllers/ConversationController.js
@@ -6,7 +6,9 @@ mainApp.controller('ConversationController', ['$scope', '$log', '$timeout', 'Con
     var synacySupportMailboxId = 21167;
 	
 	$scope.count=0;
-	$scope.testcount=0;
+	$scope.activeCount=0;
+	$scope.pendingCount=0;
+	$scope.closedCount=0;
 
     var getSynacyMailboxActiveCount = function() {
         ConversationService.getByStatusAndMailbox("active", synacySupportMailboxId).success(function(data) {
@@ -17,11 +19,16 @@ mainApp.controller('ConversationController', ['$scope', '$log', '$timeout', 'Con
 	
 	$scope.getActiveCountForMailbox = function(mailboxId) {
         ConversationService.getByStatusAndMailbox("active", mailboxId).success(function(data) {
-            $scope.testcount = data.count;
-            //$timeout(getActiveCountForMailbox, 5000);
+            $scope.activeCount = data.count;
+        });
+        ConversationService.getByStatusAndMailbox("pending", mailboxId).success(function(data) {
+            $scope.pendingCount = data.count;
+        });
+        ConversationService.getByStatusAndMailbox("closed", mailboxId).success(function(data) {
+            $scope.closedCount = data.count;
         });
     }
-	
-    getSynacyMailboxActiveCount();
+
+    $scope.getActiveCountForMailbox();
 
 }]);
