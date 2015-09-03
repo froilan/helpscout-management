0e3bb0a42b0524565efd68677b4cd75933eb2a98
Len Laurito <lenlaurito@gmail.com>
2015-09-03 23:50:35 +0800

    added unit test for Mailbox Service

--- test/MailboxServiceTest.js
+++ test/MailboxServiceTest.js
@@ -1,6 +1,6 @@
 "use strict";
 
-describe('MailboxService test', function() {
+describe("MailboxService test", function() {
 
 	var http;
 	var httpBackend;
@@ -14,7 +14,23 @@ describe('MailboxService test', function() {
   	}));
 
 
-	it('test', function() { 
+	describe("Get Mailbox List", function() { 
 
+		beforeEach(function() {
+			var url = 'https://api.helpscout.net/v1/mailboxes.json';
+			httpBackend.when('GET', url).respond(200, {
+				items: [{id: 1000, name: 'Mailbox1'}, 
+				{id: 1002, name: 'Mailbox2'}, 
+				{id: 1003, name: 'Mailbox3'}
+			]});
+
+		});
+
+		it("should get mailboxes with id and name", function(){
+			var mailboxList = mailboxService.getMailboxList();
+			httpBackend.flush();
+			expect(mailboxList[0]).toEqual({id: 1000, name:'Mailbox1'});
+			expect(mailboxList[2]).toEqual({id: 1003, name:'Mailbox3'});
+		});
 	});
 });
