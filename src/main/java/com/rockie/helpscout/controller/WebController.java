1efa6ac11eb21505adad65f7f8bfb5c8fcf588d0
Len Laurito <lenlaurito@gmail.com>
2015-09-09 04:29:28 +0800

    added controller to view dashboard

---
+++ src/main/java/com/rockie/helpscout/controller/WebController.java
@@ -0,0 +1,17 @@
+package com.rockie.helpscout.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by len on 9/8/2015.
+ */
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showDashboard() {
+        return "/pages/index.html";
+    }
+}
