e0a987a579dec185ca634d09aec807ce78aaadaf
Len Laurito <lenlaurito@gmail.com>
2015-09-10 18:44:38 +0800

    added user controller for front end

---
+++ src/main/webapp/resources/js/controllers/UserController.js
@@ -0,0 +1,12 @@
+mainApp.controller('UserController', ['$scope', '$log', '$window','UserService',function($scope, $log, $window, UserService) {
+    $scope.email;
+    $scope.password;
+
+    $scope.createUser = function() {
+        UserService.addNewUser($scope.email, $scope.password).success(function(data) {
+            alert("User has been successfully registered");
+            $window.location.href = "/helpscout-management/settings"
+        })
+    }
+
+}]);
