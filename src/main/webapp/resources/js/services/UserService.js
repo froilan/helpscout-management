3e20bebcc367d8d4cfe6691eb60a077c3679525f
Len Laurito <lenlaurito@gmail.com>
2015-09-10 18:43:51 +0800

    added user service for front end

---
+++ src/main/webapp/resources/js/services/UserService.js
@@ -0,0 +1,24 @@
+mainApp.service('UserService', function($http){
+    this.addNewUser = function(email, password) {
+        var userObject = {
+            email : email,
+            password :password
+        };
+
+        var req = {
+            method: 'POST',
+            url: '/helpscout-management/user',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(userObject)
+        }
+
+        return $http(req);
+
+        //.success(function(response) {
+        //    alert('ADDED');
+        //});
+
+    }
+});
