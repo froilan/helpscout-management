83503c8a37a8ff9a6fc37a13f112f3ed2e0c3c81
Len Laurito <lenlaurito@gmail.com>
2015-09-10 18:52:03 +0800

    added settings controller for backend

---
+++ src/main/java/com/rockie/helpscout/controller/SettingsController.java
@@ -0,0 +1,18 @@
+package com.rockie.helpscout.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by len on 9/10/2015.
+ */
+@Controller
+@RequestMapping("/settings")
+public class SettingsController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showSettingsPage() {
+        return "/pages/settings.html";
+    }
+}
