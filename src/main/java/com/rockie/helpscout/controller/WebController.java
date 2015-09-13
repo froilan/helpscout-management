cb0011432bbf97b2f3495c0f89e8a9f76976848b
Len Laurito <lenlaurito@gmail.com>
2015-09-14 03:38:12 +0800

    Updated controller to redirect to dashboard for /login if a user is logged in

--- src/main/java/com/rockie/helpscout/controller/WebController.java
+++ src/main/java/com/rockie/helpscout/controller/WebController.java
@@ -1,5 +1,8 @@
 package com.rockie.helpscout.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,5 +21,16 @@ public class WebController {
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String showRegisterPage() {
         return "/pages/register.html";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage() {
+        //If a user is already logged in, redirect to the dashboard page
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            return "redirect:";
+        }
+
+        return "/pages/login.html";
     }
 }
