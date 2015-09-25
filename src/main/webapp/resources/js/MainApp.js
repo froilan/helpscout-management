f47abc3e186e3c11942a426bbd7b480f8000c97b
Len Laurito <lenlaurito@gmail.com>
2015-09-26 01:59:52 +0800

    updated to add routings and setup of user (from cookie) upon run

--- src/main/webapp/resources/js/MainApp.js
+++ src/main/webapp/resources/js/MainApp.js
@@ -1,1 +1,34 @@
-var mainApp = angular.module("mainApp", ["ngRoute"]);
+var mainApp = angular.module("mainApp", ["ngRoute", "ngCookies"]);
+
+mainApp.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
+    $routeProvider.
+
+        when('/dashboard', {
+            templateUrl: 'pages/dashboard.html'
+        }).
+
+        when('/settings', {
+            templateUrl: 'pages/settings.html'
+        }).
+
+        when('/login', {
+            templateUrl: 'pages/login.html'
+        }).
+
+        when('/register', {
+            templateUrl: 'pages/register.html'
+        }).
+
+        otherwise({
+            redirectTo: '/dashboard'
+        });
+
+}]);
+
+mainApp.run(['$rootScope', '$http', '$cookieStore', function($rootScope, $http, $cookieStore) {
+    $rootScope.currentUser = {};
+    var currentUser = $cookieStore.get('currentUser');
+    if(currentUser) {
+        $rootScope.currentUser = currentUser;
+    }
+}]);
