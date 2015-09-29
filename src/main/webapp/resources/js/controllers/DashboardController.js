3104e59a459f64073f3b274158eed1dfb520f7cd
Len Laurito <lenlaurito@gmail.com>
2015-09-30 03:20:51 +0800

    updated dashboard controller to base dashboard on settings

--- src/main/webapp/resources/js/controllers/DashboardController.js
+++ src/main/webapp/resources/js/controllers/DashboardController.js
@@ -1,67 +1,144 @@
 /**
  * Created by froilan on 8/30/15.
  */
-mainApp.controller('DashboardController', ['$scope', '$log', '$interval', 'ConversationService', 'MailboxService',
-        function($scope, $log, $interval, ConversationService, MailboxService) {
+mainApp.controller('DashboardController', ['$scope', '$log', '$interval', '$rootScope', 'ConversationService', 'MailboxService', 'MailboxSettingService',
+    function($scope, $log, $interval, $rootScope, ConversationService, MailboxService, MailboxSettingService) {
 
-    var synacySupportMailboxId = 21167;
+    //var synacySupportMailboxId = 21167;
+    //
+    //$scope.count=0;
+    //$scope.activeCount=0;
+    //$scope.pendingCount=0;
+    //$scope.closedCount=0;
+    //$scope.mailboxId;
+    //
+    //var getSynacyMailboxActiveCount = function() {
+    //    ConversationService.getByStatusAndMailbox("active", synacySupportMailboxId).then(function(data) {
+    //        $scope.count = data.count
+    //        //$timeout(getSynacyMailboxActiveCount, 5000)
+    //    });
+    //}
+    //
+    //var updateStatusCounts = function() {
+    //    $scope.activeLoading = true;
+    //    $scope.pendingLoading = true;
+    //    $scope.closedLoading = true;
+    //
+    //    ConversationService.getByStatusAndMailbox("active", $scope.mailboxId).then(function(data) {
+    //        $scope.activeCount = data.count;
+    //    }).finally(function () {
+    //        $scope.activeLoading = false;
+    //        if(!$scope.activeLoading && !$scope.pendingLoading && !$scope.closedLoading) {
+    //            $scope.mailboxChangeLoading = false;
+    //        }
+    //    });
+    //
+    //    ConversationService.getByStatusAndMailbox("pending", $scope.mailboxId).then(function(data) {
+    //        $scope.pendingCount = data.count;
+    //    }).finally(function () {
+    //        $scope.pendingLoading = false;
+    //        if(!$scope.activeLoading && !$scope.pendingLoading && !$scope.closedLoading) {
+    //            $scope.mailboxChangeLoading = false;
+    //        }
+    //    });
+    //
+    //    ConversationService.getByStatusAndMailbox("closed", $scope.mailboxId).then(function(data) {
+    //        $scope.closedCount = data.count;
+    //    }).finally(function () {
+    //        $scope.closedLoading = false;
+    //        if(!$scope.activeLoading && !$scope.pendingLoading && !$scope.closedLoading) {
+    //            $scope.mailboxChangeLoading = false;
+    //        }
+    //    });
+    //}
+    //
+    //$scope.mailboxList = MailboxService.getMailboxList();
+    //
+    //$scope.setCurrentMailbox = function(mailboxId) {
+    //    $scope.mailboxId = mailboxId;
+    //    $scope.mailboxChangeLoading = true;
+    //    updateStatusCounts();
+    //}
+    //
+    //$scope.setCurrentMailbox(null);
+    //
+    //$interval(updateStatusCounts, 15000);
 
-    $scope.count=0;
-    $scope.activeCount=0;
-    $scope.pendingCount=0;
-    $scope.closedCount=0;
-    $scope.mailboxId;
 
-    var getSynacyMailboxActiveCount = function() {
-        ConversationService.getByStatusAndMailbox("active", synacySupportMailboxId).success(function(data) {
-            $scope.count = data.count
-            //$timeout(getSynacyMailboxActiveCount, 5000)
-        });
-    }
-
-    var updateStatusCounts = function() {
-        $scope.activeLoading = true;
-        $scope.pendingLoading = true;
-        $scope.closedLoading = true;
-
-        ConversationService.getByStatusAndMailbox("active", $scope.mailboxId).success(function(data) {
-            $scope.activeCount = data.count;
-        }).finally(function () {
-            $scope.activeLoading = false;
-            if(!$scope.activeLoading && !$scope.pendingLoading && !$scope.closedLoading) {
-                $scope.mailboxChangeLoading = false;
+        var getCurrentUser =  function() {
+            if($rootScope.currentUser && $rootScope.currentUser.user) {
+                $scope.currentUser = $rootScope.currentUser.user;
             }
-        });
+        }
 
-        ConversationService.getByStatusAndMailbox("pending", $scope.mailboxId).success(function(data) {
-            $scope.pendingCount = data.count;
-        }).finally(function () {
-            $scope.pendingLoading = false;
-            if(!$scope.activeLoading && !$scope.pendingLoading && !$scope.closedLoading) {
-                $scope.mailboxChangeLoading = false;
+        function getStatusCounts(mailboxSetting, mailbox) {
+            if(mailboxSetting.shouldShowPending) {
+                ConversationService.getByStatusAndMailbox("pending", mailboxSetting.mailboxId).then(function(response) {
+                    //var statusCount = {
+                    //    status: "Pending",
+                    //    count: data.count
+                    //}
+                    //mailbox.statusCounts.push(statusCount);
+                    mailbox.pendingCount = response.data.count;
+                    mailbox.isPendingRetrieved = true;
+                });
             }
-        });
-
-        ConversationService.getByStatusAndMailbox("closed", $scope.mailboxId).success(function(data) {
-            $scope.closedCount = data.count;
-        }).finally(function () {
-            $scope.closedLoading = false;
-            if(!$scope.activeLoading && !$scope.pendingLoading && !$scope.closedLoading) {
-                $scope.mailboxChangeLoading = false;
+            else {
+                mailbox.isPendingRetrieved = true;
             }
-        });
-    }
 
-    $scope.mailboxList = MailboxService.getMailboxList();
+            if(mailboxSetting.shouldShowActive) {
+                ConversationService.getByStatusAndMailbox("active", mailboxSetting.mailboxId).then(function(response) {
+                    //var statusCount = {
+                    //    status: "Active",
+                    //    count: data.count
+                    //}
+                    //mailbox.statusCounts.push(statusCount);
+                    mailbox.activeCount = response.data.count;
+                    mailbox.isActiveRetrieved = true;
+                });
+            }
+            else {
+                mailbox.isActiveRetrieved = true;
+            }
 
-    $scope.setCurrentMailbox = function(mailboxId) {
-        $scope.mailboxId = mailboxId;
-        $scope.mailboxChangeLoading = true;
-        updateStatusCounts();
-    }
+            if(mailboxSetting.shouldShowClosed) {
+                ConversationService.getByStatusAndMailbox("closed", mailboxSetting.mailboxId).then(function(response) {
+                    //var statusCount = {
+                    //    status: "Closed",
+                    //    count: data.count
+                    //}
+                    //mailbox.statusCounts.push(statusCount);
+                    mailbox.closedCount = response.data.count;
+                    mailbox.isClosedRetrieved = true;
+                });
+            }
+            else {
+                mailbox.isClosedRetrieved = true;
+            }
+        }
 
-    $scope.setCurrentMailbox(null);
+        var getMailboxForCurrentUser = function() {
+            MailboxSettingService.getMailboxSettingsForUser($scope.currentUser.userId).success(function(data) {
+                $scope.mailboxList = [];
+                var mailboxSettingList = data;
+                for(i = 0; i < mailboxSettingList.length; i++) {
+                    var mailboxSetting = mailboxSettingList[i];
+                    var mailbox = {
+                        mailboxName: mailboxSetting.mailboxName,
+                        //statusCounts: [],
+                        pendingCount: undefined,
+                        activeCount: undefined,
+                        closedCount: undefined
+                    }
+                    $scope.mailboxList.push(mailbox);
 
-    $interval(updateStatusCounts, 15000);
+                    getStatusCounts(mailboxSetting, mailbox);
+                }
+            });
+        }
+
+        getCurrentUser();
+        getMailboxForCurrentUser();
 
 }]);
